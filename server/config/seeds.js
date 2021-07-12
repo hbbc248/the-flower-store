130dd376a97c2f3086a367a99b10cf58dfcec10f
Ibrahim <ibrahimzerlin@hotmail.com>
2021-07-12 16:17:50 -0500

    added basic seeds to test

---
+++ server/config/seeds.js
@@ -0,0 +1,55 @@
+const db = require('./connection');
+const { User, Product, Category } = require('../models');
+
+db.once('open', async () => {
+  await Category.deleteMany();
+
+  const categories = await Category.insertMany([
+    { name: 'All' },
+    { name: 'Birthday' },
+    { name: 'Anniversary' }
+  ]);
+
+  console.log('categories seeded');
+
+  await Product.deleteMany();
+
+  const products = await Product.insertMany([
+    {
+      name: 'Dozen of red roses for Birthday',
+      description:
+        'Dozen of red roses looking very nice.',
+      image: 'one-dozen-roses.jpg',
+      category: [categories[0]._id, categories[1]._id],
+      price: 59.99
+    },
+    {
+      name: 'Dozen of red roses for Anniversary',
+      description:
+        'Dozen of red roses looking very nice.',
+      image: 'one-dozen-roses.jpg',
+      category: [categories[0]._id, categories[2]._id],
+      price: 59.99
+    }
+  ]);
+
+  console.log('products seeded');
+
+  await User.deleteMany();
+
+  await User.create({
+    firstName: 'Ibrahim',
+    lastName: 'Zerlin',
+    email: 'ibrahimzerlin@hotmail.com',
+    password: 'abcd1234',
+    orders: [
+      {
+        products: [products[0]._id, products[0]._id, products[1]._id]
+      }
+    ]
+  });
+
+  console.log('users seeded');
+
+  process.exit();
+});
