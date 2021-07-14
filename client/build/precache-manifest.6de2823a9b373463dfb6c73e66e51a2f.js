c37abc8017d0dd5964079917c58274ebfd862086
Fernanda Frers <fernanda.frers@gmail.com>
2021-07-13 19:39:22 -0500

    page seeded

---
+++ client/build/precache-manifest.6de2823a9b373463dfb6c73e66e51a2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c436377964bc4878d1d48eff7a561c0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fef0de8e57616decd29",
+    "url": "/static/css/main.ad1af64d.chunk.css"
+  },
+  {
+    "revision": "ad11eea9a28f31dc233a",
+    "url": "/static/js/2.1ece236a.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.1ece236a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fef0de8e57616decd29",
+    "url": "/static/js/main.8d17adab.chunk.js"
+  },
+  {
+    "revision": "befa59605a64ce04dc12",
+    "url": "/static/js/runtime-main.b0361512.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
--- server/config/seeds.js
+++ server/config/seeds.js
@@ -6,8 +6,10 @@ db.once('open', async () => {
 
   const categories = await Category.insertMany([
     { name: 'All' },
+    { name: 'Anniversary' },
     { name: 'Birthday' },
-    { name: 'Anniversary' }
+    { name: 'Get well' },
+    { name: 'Just Because'}
   ]);
 
   console.log('categories seeded');
@@ -16,19 +18,107 @@ db.once('open', async () => {
 
   const products = await Product.insertMany([
     {
-      name: 'Dozen of red roses for Birthday',
+      name: 'Pretty in Pastel',
       description:
-        'Dozen of red roses looking very nice.',
-      image: 'one-dozen-roses.jpg',
-      category: [categories[0]._id, categories[1]._id],
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id corporis excepturi ipsam delectus aliquid reiciendis laboriosam ea necessitatibus enim dignissimos consequuntu',
+      image: 'pretty-pastel.jpeg',
+      category: [ categories[0]._id, categories[1],categories[3]._id ],
       price: 59.99
     },
     {
-      name: 'Dozen of red roses for Anniversary',
+      name: 'Colors of the Rainbow',
+      description:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id corporis excepturi ipsam delectus aliquid reiciendis laboriosam ea necessitatibus enim dignissimos consequuntu',
+      image: 'rainbow-colors.jpeg',
+      category: [ categories[0]._id, categories[1], categories[4]._id],
+      price: 69.99
+    },
+    {
+      name: 'Spring Bouquet',
+      description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id corporis excepturi ipsam delectus aliquid reiciendis laboriosam ea necessitatibus enim dignissimos consequuntu',
+      image: 'spring.jpeg',
+      category:[ categories[0]._id, categories[1] ],
+      price: 59.99
+    },
+    {
+      name: 'Summer Bouquet',
+      description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id corporis excepturi ipsam delectus aliquid reiciendis laboriosam ea necessitatibus enim dignissimos consequuntu',
+      image: 'summer.jpeg',
+      category: [ categories[0]._id, categories[1], categories[3]._id],
+      price: 59.99
+    },
+    {
+      name: 'A Special Gift for Mom',
+      description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id corporis excepturi ipsam delectus aliquid reiciendis laboriosam ea necessitatibus enim dignissimos consequuntu',
+      image: 'mother.jpeg',
+      category: [categories[0]._id, categories[2]._id, categories[4]._id],
+      price: 59.99
+    },
+    {
+      name: 'Pretty in Pink',
+      description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id corporis excepturi ipsam delectus aliquid reiciendis laboriosam ea necessitatibus enim dignissimos consequuntu',
+      image: 'pretty-in-pink.jpeg',
+      category: [categories[0]._id, categories[2]._id, categories[3]._id],
+      price: 59.99
+    },
+    {
+      name: 'Sweet Thoughts',
+      description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id corporis excepturi ipsam delectus aliquid reiciendis laboriosam ea necessitatibus enim dignissimos consequuntu',
+      image: 'sweet-thought.jpeg',
+      category: [categories[0]._id, categories[2]._id, categories[4]._id],
+      price: 59.99
+    },
+    {
+      name: 'Violet All Over',
+      description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id corporis excepturi ipsam delectus aliquid reiciendis laboriosam ea necessitatibus enim dignissimos consequuntu',
+      image: 'violet-allover.jpeg',
+      category: [categories[0]._id, categories[2]._id],
+      price: 59.99
+    },
+    {
+      name: 'Joyful Memory',
+      description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id corporis excepturi ipsam delectus aliquid reiciendis laboriosam ea necessitatibus enim dignissimos consequuntu',
+      image: 'joyful-memory.jpeg',
+      category: [categories[0]._id, categories[3]._id, categories[2]._id],
+      price: 59.99
+    },
+    {
+      name: 'A Touch of Blue',
+      description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id corporis excepturi ipsam delectus aliquid reiciendis laboriosam ea necessitatibus enim dignissimos consequuntu',
+      image: 'touch-of-blue.jpeg',
+      category: [categories[0]._id, categories[3]._id, categories[1]._id],
+      price: 59.99
+    },
+    {
+      name: 'Bright Colors for Brighter Days',
+      description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id corporis excepturi ipsam delectus aliquid reiciendis laboriosam ea necessitatibus enim dignissimos consequuntu',
+      image: 'bright-colors.jpeg',
+      category: [categories[0]._id, categories[4]._id],
+      price: 59.99
+    },
+    {
+      name: 'Dozen of red roses',
       description:
         'Dozen of red roses looking very nice.',
       image: 'one-dozen-roses.jpg',
-      category: [categories[0]._id, categories[2]._id],
+      category: [categories[0]._id, categories[4]._id, categories[1]._id],
+      price: 59.99
+    },
+    {
+      name: 'Yellow Spring Flowers',
+      description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id corporis excepturi ipsam delectus aliquid reiciendis laboriosam ea necessitatibus enim dignissimos consequuntu',
+      image: 'yellow-spring.jpeg',
+      category: [categories[0]._id, categories[4]._id, categories[2]._id],
       price: 59.99
     }
   ]);
