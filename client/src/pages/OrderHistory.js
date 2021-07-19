5fa7bae2da35b79530a76ac9245b3e20852e557a
Ibrahim <ibrahimzerlin@hotmail.com>
2021-07-19 14:38:23 -0500

    fixed order history and stripe

--- client/src/pages/OrderHistory.js
+++ client/src/pages/OrderHistory.js
@@ -45,58 +45,73 @@ function OrderHistory() {
       const newArray = newProductsArray.map((item, i) => Object.assign({}, item, quantitiesArray[i]));
       // Get total pay for the order
       let totalPaid = 0;
-      for (let i=0; i < newArray.length ; i++) {
+      for (let i = 0; i < newArray.length; i++) {
         totalPaid = totalPaid + (newArray[i].price * newArray[i].purchaseQuantity);
       }
-      const newTotalPaid =  totalPaid.toFixed(2);
+      const newTotalPaid = totalPaid.toFixed(2);
       // blend old array, and push newArray and total paid into it
       const newSigleOrder = Object.assign({}, singleOrder, { productsNew: newArray, totalPaid: newTotalPaid })
       // Push final order array into newOrders array.
       newOrders.push(newSigleOrder);
     }
   });
-  
+
+  console.log(newOrders);
+
   return (
     <>
       <div className="container my-1">
         <Link to="/">← Back to Products</Link>
-
         {user ? (
           <>
-
             <h2>
               Order History for {user.firstName} {user.lastName}
             </h2>
-            {newOrders.map((order) => (
-              <div>
-                <div className="flex-row accordion" key={order._id} id="accordionExample" >
-                  <div className="card">
-                <button type="button" className="collapsible" data-toggle="collapse" data-target="#data" aria-expanded="true" aria-controls="collapseOne">Order Date: {new Date(parseInt(order.purchaseDate)).toLocaleDateString()}</button>
-                  {order.productsNew.map(({ _id, image, name, price, purchaseQuantity }, index) => (
-                    <div key={index} className="collapse show content" id="data" data-parent="#accordionExample">
-                      <div className="card-body">
-                      <Link to={`/products/${_id}`}>
-                        <img className="card-img-top" alt={name} src={`/images/${image}`} />
-                        <h2>{name}</h2>
-                      </Link>
-                      <ul className="list-group list-group-flush">
-                        <li className="list-group-item">Price: ${price}</li>
-                        <li className="list-group-item">Purchased Quantity:{purchaseQuantity}</li>
-                        <li className="list-group-item">Total Paid: {order.totalPaid}</li>
-                        <li className="list-group-item">Send to: {order.shipTo}</li>
-                        <li className="list-group-item">Shipping address: {order.shipToAddress}</li>
-                        <li className="list-group-item">Message: {order.message}</li>
-                      </ul>
+            <div className="flex-row accordion" id="accordionExample" >
+              {newOrders.map((order) => (
+                <div key={order._id} className="card">
+                  <div className="card-header" id="headingTwo">
+                    <h2 className="mb-0">
+                      <button className="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target={"#collapse" + order._id} aria-expanded="true" aria-controls="collapse">
+                        Order Placed On: {new Date(parseInt(order.purchaseDate)).toLocaleDateString()}
+                      </button>
+                    </h2>
+                  </div>
+                  <div id={"collapse" + order._id} className="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+                    <div className="card-body">
+                      <h5 className="card-title text-center">
+                        Order Details:
+                      </h5>
+                      {order.productsNew.map((product) => (
+                        <div key={product._id} className="row border-top pt-2 mb-2 mx-3 align-items-center">
+                          <Link to={`/products/${product._id}`} className="col-sm-3">
+                            <img className="card-img-left" alt={product.name} src={`/images/${product.image}`} />
+                          </Link>
+                          <div className="col-sm-5">
+                            <Link to={`/products/${product._id}`} className="col-md-3">
+                              <h5 className="text-center">{product.name}</h5>
+                            </Link>
+                          </div>
+                          <div className="col-sm-2">
+                            <h6 className="text-center">Qty: {product.purchaseQuantity}</h6>
+                          </div>
+                          <div className="col-sm-2">
+                            <h6 className="text-center">${product.price} Each</h6>
+                          </div>
+                        </div>
+                      ))}
+                      <div className="list-group-item">
+                        <h6 className="text-right mr-4 my-2">Total Paid: ${order.totalPaid}</h6>
+                        <h6 className="">Send to: {order.shipTo}</h6>
+                        <h6 className="">Shipping address: {order.shipToAddress}</h6>
+                        <h6 className="">Personalized message: "{order.message}"</h6>
                       </div>
-                      </div>
-                  ))}
-                   </div>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            ))}
-
+              ))}
+            </div>
           </>
-
         ) : null}
       </div>
       <Cart />
